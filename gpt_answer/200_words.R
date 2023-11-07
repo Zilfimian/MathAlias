19bf60a9d4f5f48f461574bfd486ca6c924974c7
Zilfimian <lusine.zilfimian@mail.ru>
2023-11-07 14:04:28 +0400

    Add files via upload

---
+++ gpt_answer/200_words.R
@@ -0,0 +1,37 @@
+library(stringr)
+library(dplyr)
+
+Sys.setenv("OPENAI_API_KEY" = "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")
+
+load("Names_data_old.rda")
+Names_data_new <- NULL
+
+for(i in 1:10){
+  # Get response from OpenAI "gpt-3.5-turbo"
+  response <- openai::create_completion(model = "text-davinci-003",
+                                        #Input Your Prompt
+                                        prompt = 'system clear \n I want to create an app like Alias but only using terms from mathematics. I need 1000 full names of the most famouts mathematician and math related person for my game. Also year of birth and death',
+                                        temperature = 1,
+                                        max_tokens = 3900)
+  
+  Names <- response$choices$text %>% str_split(pattern = "\n?\n[0-9]*\\.\\s+", simplify = T) %>% t() %>% c()
+  Names_data <- Names %>% str_split(pattern = "\\(", simplify = T) 
+  Names_data[,1] <- trimws(Names_data[,1])
+  Names_data[,2] <- str_remove_all(Names_data[,2], pattern = "\\)")
+  
+  Names_data_new<- rbind(Names_data_new, Names_data)  
+}
+
+Names_data_old <- rbind(Names_data_old, Names_data_new)
+
+Names_data_old <- data.frame(Names_data_old)%>% filter(nchar(X1)<20) %>% distinct(X1,.keep_all = T)
+
+save(Names_data_old, file = "Names_data_old.rda")
+
+
+Names_data_old <- data.frame(en=Names_data_old$X1, year = Names_data_old$X2)
+
+Names_data_old$hy <- Names_data_old$en %>% lapply( function(x) translate_Google(x, sourceLang="en", targetLang="hy") ) %>% unlist()
+Names_data_old$ru <- Names_data_old$en %>% lapply( function(x) translate_Google(x, sourceLang="en", targetLang="ru") ) %>% unlist()
+
+Names_data_old %>% writexl::write_xlsx("Names_data_old.xlsx")
---
+++ gpt_answer/300_objects.R
@@ -0,0 +1,36 @@
+library(stringr)
+library(dplyr)
+source("Translation.R")
+
+Sys.setenv("OPENAI_API_KEY" = "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")
+
+load("Objects_old.rda")
+
+  # Get response from OpenAI "gpt-3.5-turbo"
+  response <- openai::create_completion(model = "text-davinci-003",
+                                        #Input Your Prompt
+                                        prompt = 'system clear \n List 1000 mathematical 1-word Object different names.',
+                                        temperature = 1,
+                                        max_tokens = 3900)
+  
+  Objects <- response$choices$text %>% str_split(pattern = "\n[0-9]*\\.\\s?", simplify = T) %>% t() %>% c()
+  Objects
+
+  #Objects <- Objects_old[169]%>% str_split(pattern = ",|:|\n", simplify = T)%>% t() %>% c()
+  Objects_old <- c(Objects, Objects_old)
+  Objects_old <- unique(trimws(Objects_old))
+  Objects_old <- Objects_old[nchar(Objects_old)>2]
+  length(Objects_old)
+  #which(nchar(Objects_old)>100)
+  
+  save(Objects_old, file = "Objects_old.rda")
+
+  
+  
+  Objects_old_df <- data.frame(en=Objects_old, hy= Objects_old)
+  
+  Objects_old_df$hy <- Objects_old_df$en %>% lapply( function(x) translate_Google(x, sourceLang="en", targetLang="hy") ) %>% unlist()
+  Objects_old_df$ru <- Objects_old_df$en %>% lapply( function(x) translate_Google(x, sourceLang="en", targetLang="ru") ) %>% unlist()
+  
+  Objects_old_df %>% writexl::write_xlsx("Objects.xlsx")
+  
